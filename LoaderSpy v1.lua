9ddbb41adda95e2f3c032f1515073763946a8c6f
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-08-11 22:02:13 +0300

    Update LoaderSpy v1.lua

--- LoaderSpy v1.lua
+++ LoaderSpy v1.lua
@@ -1,12 +1,20 @@
 -- Spies on load & loadstring calls v1
-local target=[[
+local target=[=[
 
     
     
-]]
+]=]
+
 local LD1,LD2=load,loadstring
-load=function(c,...) print("load:",c) return LD1(c,...) end
+load=function(c,...) 
+    print("load:",c)
+    return LD1(c,...)
+end
+
 if LD2 then 
-loadstring=function(c,...) print("loadstring:",c) return LD2(c,...) end
+    loadstring=function(c,...)
+        print("loadstring:",c)
+        return LD2(c,...)
+    end
 end
 load(target)(...)
