f7273c3a5d3e83f236ae72da3b672a88bb102c2f
Prodgc <162835899+Prodgc@users.noreply.github.com>
2025-08-11 21:55:50 +0300

    Update and rename LoaderSpy.lua to LoaderSpy v1.lua

---
+++ LoaderSpy v1.lua
@@ -0,0 +1,12 @@
+-- Spies on load & loadstring calls v1
+local target=[[
+
+    
+    
+]]
+local LD1,LD2=load,loadstring
+load=function(c,...) print("load:",c) return LD1(c,...) end
+if LD2 then 
+loadstring=function(c,...) print("loadstring:",c) return LD2(c,...) end
+end
+load(target)(...)
